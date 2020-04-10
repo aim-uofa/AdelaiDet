3a34e1bfa4a1d142c2f87dd451d2441630390c67
Zhi Tian <tianzhi0549@163.com>
2020-04-11 02:08:31 +0930

    del data['weight_order']

--- adet/checkpoint/adet_checkpoint.py
+++ adet/checkpoint/adet_checkpoint.py
@@ -22,6 +22,8 @@ class AdetCheckpointer(DetectionCheckpointer):
                     # Detection models have "blobs", but ImageNet models don't
                     data = data["blobs"]
                 data = {k: v for k, v in data.items() if not k.endswith("_momentum")}
+                if "weight_order" in data:
+                    del data["weight_order"]
                 return {"model": data, "__author__": "Caffe2", "matching_heuristics": True}
 
         loaded = super()._load_file(filename)  # load native pth checkpoint
