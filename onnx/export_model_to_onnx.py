1e52c9694c66689f18adc625ceb806b0908344f9
chenp <chenp@own>
2020-05-23 13:45:59 +0930

    add onnx convert support and verify it in onnxruntime for FCOS/Blendmask

--- onnx/export_model_to_onnx.py
+++ onnx/export_model_to_onnx.py
@@ -5,6 +5,9 @@ python onnx/export_model_to_onnx.py \
     --output /data/pretrained/onnx/fcos/FCOS_R_50_1x_bn_head.onnx
     --opts MODEL.WEIGHTS /data/pretrained/pytorch/fcos/FCOS_R_50_1x_bn_head.pth MODEL.FCOS.NORM "BN"
 
+# about the upsample/interpolate
+https://github.com/pytorch/pytorch/issues/10446
+https://github.com/pytorch/pytorch/issues/18113
 """
 
 import argparse
@@ -34,18 +37,105 @@ from detectron2.utils.logger import setup_logger
 from detectron2.data import MetadataCatalog
 from detectron2.modeling import build_model
 from detectron2.checkpoint import DetectionCheckpointer
-from collections import OrderedDict
+from detectron2.modeling import ProposalNetwork
 
 from adet.config import get_cfg
+from adet.modeling import FCOS, BlendMask
 
-class FCOS(torch.nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-        self.in_features          = cfg.MODEL.FCOS.IN_FEATURES
+def patch_blendmask(cfg, model, output_names):
+    def forward(self, tensor):
+        images = None
+        gt_instances = None
+        basis_sem = None
 
-    def forward(self, features):
+        features = self.backbone(tensor)
+        basis_out, basis_losses = self.basis_module(features, basis_sem)
+        proposals, proposal_losses = self.proposal_generator(images, features, gt_instances, self.top_layer)
+        return basis_out["bases"], proposals
+
+    model.forward = types.MethodType(forward, model)
+    # output
+    output_names.extend(["bases"])
+    for item in ["logits", "bbox_reg", "centerness", "top_feats"]:
+        for l in range(len(cfg.MODEL.FCOS.FPN_STRIDES)):
+            fpn_name = "P{}".format(3 + l)
+            output_names.extend([fpn_name + item])
+
+def patch_ProposalNetwork(cfg, model, output_names):
+    def forward(self, tensor):
+        images = None
+        gt_instances = None
+
+        features = self.backbone(tensor)
+        proposals, proposal_losses = self.proposal_generator(images, features, gt_instances)
+        return proposals
+
+    model.forward = types.MethodType(forward, model)
+    # output
+    for item in ["logits", "bbox_reg", "centerness"]:
+        for l in range(len(cfg.MODEL.FCOS.FPN_STRIDES)):
+            fpn_name = "P{}".format(3 + l)
+            output_names.extend([fpn_name + item])
+
+def patch_fcos(cfg, proposal_generator):
+    def proposal_generator_forward(self, images, features, gt_instances=None, top_module=None):
         features = [features[f] for f in self.in_features]
-        return features
+        logits_pred, reg_pred, ctrness_pred, top_feats, bbox_towers = self.fcos_head(features, top_module, self.yield_proposal)
+        return (logits_pred, reg_pred, ctrness_pred, top_feats, bbox_towers), None
+
+    proposal_generator.forward = types.MethodType(proposal_generator_forward, proposal_generator)
+
+def patch_fcos_head(cfg, fcos_head):
+    # step 1. config
+    norm = None if cfg.MODEL.FCOS.NORM == "none" else cfg.MODEL.FCOS.NORM
+    head_configs = {"cls": (cfg.MODEL.FCOS.NUM_CLS_CONVS,
+                            cfg.MODEL.FCOS.USE_DEFORMABLE),
+                    "bbox": (cfg.MODEL.FCOS.NUM_BOX_CONVS,
+                             cfg.MODEL.FCOS.USE_DEFORMABLE),
+                    "share": (cfg.MODEL.FCOS.NUM_SHARE_CONVS,
+                              False)}
+
+    # step 2. seperate module
+    for l in range(fcos_head.num_levels):
+        for head in head_configs:
+            tower = []
+            num_convs, use_deformable = head_configs[head]
+            for i in range(num_convs):
+                tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 0]))
+                if norm in ["GN", "NaiveGN"]:
+                    tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 1]))
+                elif norm in ["BN", "SyncBN"]:
+                    tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 1][l]))
+                tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 2]))
+            fcos_head.add_module('{}_tower{}'.format(head, l), torch.nn.Sequential(*tower))
+
+    # step 3. override fcos_head forward
+    def fcos_head_forward(self, x, top_module=None, yield_bbox_towers=False):
+        logits = []
+        bbox_reg = []
+        ctrness = []
+        top_feats = []
+        bbox_towers = []
+        for l, feature in enumerate(x):
+            feature = self.share_tower(feature)
+            cls_tower = getattr(self, 'cls_tower{}'.format(l))(feature)
+            bbox_tower = getattr(self, 'bbox_tower{}'.format(l))(feature)
+            if yield_bbox_towers:
+                bbox_towers.append(bbox_tower)
+
+            logits.append(self.cls_logits(cls_tower))
+            ctrness.append(self.ctrness(bbox_tower))
+            reg = self.bbox_pred(bbox_tower)
+            if self.scales is not None:
+                reg = self.scales[l](reg)
+            # Note that we use relu, as in the improved FCOS, instead of exp.
+            bbox_reg.append(F.relu(reg))
+            if top_module is not None:
+                top_feats.append(top_module(bbox_tower))
+
+        return logits, bbox_reg, ctrness, top_feats, bbox_towers
+
+    fcos_head.forward = types.MethodType(fcos_head_forward, fcos_head)
 
 def main():
     parser = argparse.ArgumentParser(description="Export model to the onnx format")
@@ -79,6 +169,8 @@ def main():
     # norm for ONNX: change FrozenBN back to BN
     cfg.MODEL.BACKBONE.FREEZE_AT = 0
     cfg.MODEL.RESNETS.NORM = "BN"
+    cfg.MODEL.BASIS_MODULE.NORM = "BN"
+
     # turn on the following configuration according to your own convenience
     #cfg.MODEL.FCOS.NORM = "BN"
     #cfg.MODEL.FCOS.NORM = "NaiveGN"
@@ -98,61 +190,6 @@ def main():
     _ = checkpointer.load(cfg.MODEL.WEIGHTS)
     logger.info("load Model:\n{}".format(cfg.MODEL.WEIGHTS))
 
-    # patch fcos_head
-    # step 1. config
-    fcos_head = model.proposal_generator.fcos_head
-    norm = None if cfg.MODEL.FCOS.NORM == "none" else cfg.MODEL.FCOS.NORM
-    head_configs = {"cls": (cfg.MODEL.FCOS.NUM_CLS_CONVS,
-                            cfg.MODEL.FCOS.USE_DEFORMABLE),
-                    "bbox": (cfg.MODEL.FCOS.NUM_BOX_CONVS,
-                             cfg.MODEL.FCOS.USE_DEFORMABLE),
-                    "share": (cfg.MODEL.FCOS.NUM_SHARE_CONVS,
-                              False)}
-
-    # step 2. seperate module
-    for l in range(fcos_head.num_levels):
-        for head in head_configs:
-            tower = []
-            num_convs, use_deformable = head_configs[head]
-            for i in range(num_convs):
-                tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 0]))
-                if norm in ["GN", "NaiveGN"]:
-                    tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 1]))
-                elif norm in ["BN", "SyncBN"]:
-                    tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 1][l]))
-                tower.append(deepcopy(getattr(fcos_head, '{}_tower'.format(head))[i*3 + 2]))
-            fcos_head.add_module('{}_tower{}'.format(head, l), torch.nn.Sequential(*tower))
-
-    # step 3. override forward
-    def fcos_head_forward(self, x):
-        logits = []
-        bbox_reg = []
-        ctrness = []
-        bbox_towers = []
-        for l, feature in enumerate(x):
-            feature = self.share_tower(feature)
-            cls_tower = getattr(self, 'cls_tower{}'.format(l))(feature)
-            bbox_tower = getattr(self, 'bbox_tower{}'.format(l))(feature)
-
-            logits.append(self.cls_logits(cls_tower))
-            ctrness.append(self.ctrness(bbox_tower))
-            reg = self.bbox_pred(bbox_tower)
-            if self.scales is not None:
-                reg = self.scales[l](reg)
-            # Note that we use relu, as in the improved FCOS, instead of exp.
-            bbox_reg.append(F.relu(reg))
-
-        return logits, bbox_reg, ctrness, bbox_towers
-
-    fcos_head.forward = types.MethodType(fcos_head_forward, fcos_head)
-
-    proposal_generator = FCOS(cfg)
-    onnx_model = torch.nn.Sequential(OrderedDict([
-        ('backbone', model.backbone),
-        ('proposal_generator', proposal_generator),
-        ('heads', model.proposal_generator.fcos_head),
-    ]))
-
     height, width = 800, 1088
     if args.width > 0:
         width = args.width
@@ -161,13 +198,19 @@ def main():
     input_names = ["input_image"]
     dummy_input = torch.zeros((1, 3, height, width)).to(cfg.MODEL.DEVICE)
     output_names = []
-    for item in ["logits", "bbox_reg", "centerness"]:
-        for l in range(len(cfg.MODEL.FCOS.FPN_STRIDES)):
-            fpn_name = "P{}".format(3 + l)
-            output_names.extend([fpn_name + item])
+    if isinstance(model, BlendMask):
+        patch_blendmask(cfg, model, output_names)
+
+    if isinstance(model, ProposalNetwork):
+        patch_ProposalNetwork(cfg, model, output_names)
+
+    if hasattr(model, 'proposal_generator'):
+        if isinstance(model.proposal_generator, FCOS):
+            patch_fcos(cfg, model.proposal_generator)
+            patch_fcos_head(cfg, model.proposal_generator.fcos_head)
 
     torch.onnx.export(
-        onnx_model,
+        model,
         dummy_input,
         args.output,
         verbose=True,
