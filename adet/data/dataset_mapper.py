e15f40041b27d559f03ee5f997084402ed997de2
Yuliang Liu <34134635+Yuliang-Liu@users.noreply.github.com>
2020-07-02 07:52:20 +0930

    update dataset mapper with latest detectron2

--- adet/data/dataset_mapper.py
+++ adet/data/dataset_mapper.py
@@ -35,7 +35,10 @@ class DatasetMapperWithBasis(DatasetMapper):
     def __init__(self, cfg, is_train=True):
         super().__init__(cfg, is_train)
         
-        self.crop = self.augmentation[0]
+        if cfg.INPUT.CROP.ENABLED and is_train:
+            self.crop = self.augmentation[0]
+        else:
+            self.crop = None
         # Rebuild augmentations
         logger.info("Rebuilding the augmentations. The previous augmentations will be overridden.")
         self.augmentation = build_augmentation(cfg, is_train)
