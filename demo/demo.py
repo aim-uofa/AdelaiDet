ec45d466b05991bd5b6820368d65112b06f08aec
Zhi Tian <tianzhi0549@163.com>
2021-03-05 19:54:25 +1030

    changing default confidence score to 0.3

--- demo/demo.py
+++ demo/demo.py
@@ -52,7 +52,7 @@ def get_parser():
     parser.add_argument(
         "--confidence-threshold",
         type=float,
-        default=0.5,
+        default=0.3,
         help="Minimum score for instance predictions to be shown",
     )
     parser.add_argument(
