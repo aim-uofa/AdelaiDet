a73f385dcad60a3a0e36e543faa7dcf4ca0b3141
Zhi Tian <tianzhi0549@163.com>
2021-03-14 12:59:58 +1030

    add voc

--- adet/data/builtin.py
+++ adet/data/builtin.py
@@ -2,6 +2,8 @@ import os
 
 from detectron2.data.datasets.register_coco import register_coco_instances
 from detectron2.data.datasets.builtin_meta import _get_builtin_metadata
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets.coco import load_coco_json, load_sem_seg
 
 from .datasets.text import register_text_instances
 
@@ -30,6 +32,41 @@ metadata_text = {
     "thing_classes": ["text"]
 }
 
+_PREDEFINED_BBTP_VOC = {
+    "bbtp_voc_train": ("VOC2012/JPEGImages", "VOC2012/voc_2012_train_aug_cocostyle.json"),
+    "bbtp_voc_val": ("VOC2012/JPEGImages", "VOC2012/voc_2012_val_cocostyle.json"),
+}
+
+
+def register_bbtp_voc_instances(name, metadata, json_file, image_root):
+    """
+    Register a dataset in COCO's json annotation format for
+    instance detection, instance segmentation and keypoint detection.
+    (i.e., Type 1 and 2 in http://cocodataset.org/#format-data.
+    `instances*.json` and `person_keypoints*.json` in the dataset).
+
+    This is an example of how to register a new dataset.
+    You can do something similar to this function, to register new datasets.
+
+    Args:
+        name (str): the name that identifies a dataset, e.g. "coco_2014_train".
+        metadata (dict): extra metadata associated with this dataset.  You can
+            leave it as an empty dict.
+        json_file (str): path to the json instance annotation file.
+        image_root (str or path-like): directory which contains all the images.
+    """
+    assert isinstance(name, str), name
+    assert isinstance(json_file, (str, os.PathLike)), json_file
+    assert isinstance(image_root, (str, os.PathLike)), image_root
+    # 1. register a function which returns dicts
+    DatasetCatalog.register(name, lambda: load_coco_json(json_file, image_root, name))
+
+    # 2. Optionally, add metadata about this dataset,
+    # since they might be useful in evaluation, visualization or logging
+    MetadataCatalog.get(name).set(
+        json_file=json_file, image_root=image_root, evaluator_type="coco", **metadata
+    )
+
 
 def register_all_coco(root="datasets"):
     for key, (image_root, json_file) in _PREDEFINED_SPLITS_PIC.items():
@@ -48,6 +85,13 @@ def register_all_coco(root="datasets"):
             os.path.join(root, json_file) if "://" not in json_file else json_file,
             os.path.join(root, image_root),
         )
+    for key, (image_root, json_file) in _PREDEFINED_BBTP_VOC.items():
+        # Assume pre-defined datasets live in `./datasets`.
+        register_bbtp_voc_instances(
+            key, {},
+            os.path.join(root, json_file) if "://" not in json_file else json_file,
+            os.path.join(root, image_root),
+        )
 
-
-register_all_coco()
+_root = os.getenv("DETECTRON2_DATASETS", "datasets")
+register_all_coco(_root)
