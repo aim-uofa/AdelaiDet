bb6dda936c3204f7eda84d6d71d1b141dcd5cf38
Zhi Tian <tianzhi0549@163.com>
2021-03-14 13:05:59 +1030

    using coco register

--- adet/data/builtin.py
+++ adet/data/builtin.py
@@ -38,36 +38,6 @@ _PREDEFINED_BBTP_VOC = {
 }
 
 
-def register_bbtp_voc_instances(name, metadata, json_file, image_root):
-    """
-    Register a dataset in COCO's json annotation format for
-    instance detection, instance segmentation and keypoint detection.
-    (i.e., Type 1 and 2 in http://cocodataset.org/#format-data.
-    `instances*.json` and `person_keypoints*.json` in the dataset).
-
-    This is an example of how to register a new dataset.
-    You can do something similar to this function, to register new datasets.
-
-    Args:
-        name (str): the name that identifies a dataset, e.g. "coco_2014_train".
-        metadata (dict): extra metadata associated with this dataset.  You can
-            leave it as an empty dict.
-        json_file (str): path to the json instance annotation file.
-        image_root (str or path-like): directory which contains all the images.
-    """
-    assert isinstance(name, str), name
-    assert isinstance(json_file, (str, os.PathLike)), json_file
-    assert isinstance(image_root, (str, os.PathLike)), image_root
-    # 1. register a function which returns dicts
-    DatasetCatalog.register(name, lambda: load_coco_json(json_file, image_root, name))
-
-    # 2. Optionally, add metadata about this dataset,
-    # since they might be useful in evaluation, visualization or logging
-    MetadataCatalog.get(name).set(
-        json_file=json_file, image_root=image_root, evaluator_type="coco", **metadata
-    )
-
-
 def register_all_coco(root="datasets"):
     for key, (image_root, json_file) in _PREDEFINED_SPLITS_PIC.items():
         # Assume pre-defined datasets live in `./datasets`.
@@ -87,11 +57,12 @@ def register_all_coco(root="datasets"):
         )
     for key, (image_root, json_file) in _PREDEFINED_BBTP_VOC.items():
         # Assume pre-defined datasets live in `./datasets`.
-        register_bbtp_voc_instances(
+        register_coco_instances(
             key, {},
             os.path.join(root, json_file) if "://" not in json_file else json_file,
             os.path.join(root, image_root),
         )
+
 
 _root = os.getenv("DETECTRON2_DATASETS", "datasets")
 register_all_coco(_root)
