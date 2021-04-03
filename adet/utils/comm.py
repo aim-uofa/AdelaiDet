34475e8bd5c9e47627cdfaa59c67878c7b4f0d7f
Tian Zhi <zhi.tian@adelaide.edu.au>
2021-04-03 19:04:34 +1030

    Update comm.py

--- adet/utils/comm.py
+++ adet/utils/comm.py
@@ -17,7 +17,7 @@ def reduce_sum(tensor):
 def reduce_mean(tensor):
     num_gpus = get_world_size()
     total = reduce_sum(tensor)
-    return total / num_gpus
+    return float(total) / num_gpus
 
 
 def aligned_bilinear(tensor, factor):
