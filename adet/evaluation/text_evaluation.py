83e3f81b305d93f896bc313a732d69d09412126e
Yuliang-Liu <liu.yuliang@mail.scut.edu.cn>
2021-09-21 10:57:41 +0800

    m1

--- adet/evaluation/text_evaluation.py
+++ adet/evaluation/text_evaluation.py
@@ -21,7 +21,7 @@ import shutil
 from shapely.geometry import Polygon, LinearRing
 from adet.evaluation import text_eval_script
 import zipfile
-
+import pickle
 
 class TextEvaluator(DatasetEvaluator):
     """
@@ -41,6 +41,15 @@ class TextEvaluator(DatasetEvaluator):
             raise AttributeError(
                 f"json_file was not found in MetaDataCatalog for '{dataset_name}'."
             )
+        
+        self.voc_size = cfg.MODEL.BATEXT.VOC_SIZE
+        self.use_customer_dictionary = cfg.MODEL.BATEXT.CUSTOM_DICT
+        if not self.use_customer_dictionary:
+            self.CTLABELS = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
+        else:
+            with open(self.use_customer_dictionary, 'rb') as fp:
+                self.CTLABELS = pickle.load(fp)
+        assert(int(self.voc_size - 1) == len(self.CTLABELS)), "voc_size is not matched dictionary size, got {} and {}.".format(int(self.voc_size - 1), len(self.CTLABELS))
 
         json_file = PathManager.get_local_path(self._metadata.json_file)
         with contextlib.redirect_stdout(io.StringIO()):
@@ -53,6 +62,8 @@ class TextEvaluator(DatasetEvaluator):
         elif "ctw1500" in dataset_name:
             self._text_eval_gt_path = "datasets/evaluation/gt_ctw1500.zip"
             self._word_spotting = False
+        else:
+            self._text_eval_gt_path = ""
         self._text_eval_confidence = cfg.MODEL.FCOS.INFERENCE_TH_TEST
 
     def reset(self):
@@ -63,7 +74,7 @@ class TextEvaluator(DatasetEvaluator):
             prediction = {"image_id": input["image_id"]}
 
             instances = output["instances"].to(self._cpu_device)
-            prediction["instances"] = instances_to_coco_json(instances, input["image_id"])
+            prediction["instances"] = self.instances_to_coco_json(instances, input["image_id"])
             self._predictions.append(prediction)
 
     def to_eval_format(self, file_path, temp_dir="temp_det_results", cf_th=0.5):
@@ -203,6 +214,9 @@ class TextEvaluator(DatasetEvaluator):
 
         self._results = OrderedDict()
         
+        if not self._text_eval_gt_path:
+            
+            return copy.deepcopy(self._results)
         # eval text
         temp_dir = "temp_det_results/"
         self.to_eval_format(file_path, temp_dir, self._text_eval_confidence)
@@ -220,72 +234,75 @@ class TextEvaluator(DatasetEvaluator):
         return copy.deepcopy(self._results)
 
 
-def instances_to_coco_json(instances, img_id):
-    num_instances = len(instances)
-    if num_instances == 0:
-        return []
-
-    scores = instances.scores.tolist()
-    beziers = instances.beziers.numpy()
-    recs = instances.recs.numpy()
-
-    results = []
-    for bezier, rec, score in zip(beziers, recs, scores):
-        # convert beziers to polygons
-        poly = bezier_to_polygon(bezier)
-        s = decode(rec)
-        result = {
-            "image_id": img_id,
-            "category_id": 1,
-            "polys": poly,
-            "rec": s,
-            "score": score,
-        }
-        results.append(result)
-    return results
-
-
-def bezier_to_polygon(bezier):
-    u = np.linspace(0, 1, 20)
-    bezier = bezier.reshape(2, 4, 2).transpose(0, 2, 1).reshape(4, 4)
-    points = np.outer((1 - u) ** 3, bezier[:, 0]) \
-        + np.outer(3 * u * ((1 - u) ** 2), bezier[:, 1]) \
-        + np.outer(3 * (u ** 2) * (1 - u), bezier[:, 2]) \
-        + np.outer(u ** 3, bezier[:, 3])
+    def instances_to_coco_json(self, instances, img_id):
+        num_instances = len(instances)
+        if num_instances == 0:
+            return []
     
-    # convert points to polygon
-    points = np.concatenate((points[:, :2], points[:, 2:]), axis=0)
-    return points.tolist()
+        scores = instances.scores.tolist()
+        beziers = instances.beziers.numpy()
+        recs = instances.recs.numpy()
+    
+        results = []
+        for bezier, rec, score in zip(beziers, recs, scores):
+            # convert beziers to polygons
+            poly = self.bezier_to_polygon(bezier)
+            s = self.decode(rec)
+            result = {
+                "image_id": img_id,
+                "category_id": 1,
+                "polys": poly,
+                "rec": s,
+                "score": score,
+            }
+            results.append(result)
+        return results
 
 
-CTLABELS = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
+    def bezier_to_polygon(self, bezier):
+        u = np.linspace(0, 1, 20)
+        bezier = bezier.reshape(2, 4, 2).transpose(0, 2, 1).reshape(4, 4)
+        points = np.outer((1 - u) ** 3, bezier[:, 0]) \
+            + np.outer(3 * u * ((1 - u) ** 2), bezier[:, 1]) \
+            + np.outer(3 * (u ** 2) * (1 - u), bezier[:, 2]) \
+            + np.outer(u ** 3, bezier[:, 3])
+        
+        # convert points to polygon
+        points = np.concatenate((points[:, :2], points[:, 2:]), axis=0)
+        return points.tolist()
 
-
-def ctc_decode(rec):
-    # ctc decoding
-    last_char = False
-    s = ''
-    for c in rec:
-        c = int(c)
-        if c < 95:
-            if last_char != c:
-                s += CTLABELS[c]
-                last_char = c
-        elif c == 95:
-            s += u'口'
-        else:
-            last_char = False
-    return s
-
-
-def decode(rec):
-    s = ''
-    for c in rec:
-        c = int(c)
-        if c < 95:
-            s += CTLABELS[c]
-        elif c == 95:
-            s += u'口'
-
-    return s
+    def ctc_decode(self, rec):
+        # ctc decoding
+        last_char = False
+        s = ''
+        for c in rec:
+            c = int(c)
+            if c < self.voc_size - 1:
+                if last_char != c:
+                    if self.voc_size == 96:
+                        s += self.CTLABELS[c]
+                        last_char = c
+                    else:
+                        s += str(chr(self.CTLABELS[c]))
+                        last_char = c
+            elif c == self.voc_size -1:
+                s += u'口'
+            else:
+                last_char = False
+        return s
+    
+    
+    def decode(self, rec):
+        s = ''
+        for c in rec:
+            c = int(c)
+            if c < self.voc_size - 1:
+                if self.voc_size ==96:
+                    s += self.CTLABELS[c]
+                else:
+                    s += str(chr(self.CTLABELS[c]))
+            elif c == self.voc_size -1:
+                s += u'口'
+    
+        return s
             
